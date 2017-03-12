c6c31a1c008281c66cb694c8d6ea7e9c5f76f01d
Sparx92 <stefankoserkov@gmail.com>
2017-03-12 13:47:50 +0200

    Fixed backoffice Login page

--- Src/Web/DotLms.Web/App_Start/BundleConfig.cs
+++ Src/Web/DotLms.Web/App_Start/BundleConfig.cs
@@ -19,7 +19,7 @@ namespace DotLms.Web
             // ready for production, use the build tool at http://modernizr.com to pick only the tests you need.
             bundles.Add(new ScriptBundle("~/bundles/modernizr").Include(
                         "~/Scripts/modernizr-*"));
-            
+
 
             // tinyMce wysiwg editor
             bundles.Add(new ScriptBundle("~/bundles/tinymce")
@@ -28,16 +28,33 @@ namespace DotLms.Web
 
             // Unify template
 
-            bundles.Add(new StyleBundle("~/bundles/unify/css")
+            bundles.Add(new StyleBundle("~/bundles/unify/backoffice/logincss")
+                .Include("~/Areas/Backoffice/Assets/css/pages/page_log_reg_v2.css")
+                );
+
+            bundles.Add(new ScriptBundle("~/bundles/unify/backoffice/loginjs")
+                .Include("~/Areas/Backoffice/Assets/plugins/backstretch/jquery.backstretch.min.js")
+                .Include("~/Areas/Backoffice/Assets/js/login-page.js")
+                );
+
+            bundles.Add(new StyleBundle("~/bundles/unify/backoffice/css")
                 .Include("~/Areas/Backoffice/Assets/plugins/bootstrap/css/bootstrap.min.css")
                 .Include("~/Areas/Backoffice/Assets/css/style.css")
-                .Include("~/Areas/Backoffice/Assets/css/headers/header-default.css"));
-            
+                .Include("~/Areas/Backoffice/Assets/css/headers/header-default.css")
+                .Include("~/Areas/Backoffice/Assets/plugins/animate.css")
+                .Include("~/Areas/Backoffice/Assets/css/theme-colors/default.css")
+                .Include("~/Areas/Backoffice/Assets/css/theme-skins/dark.css")
+                .Include("~/Areas/Backoffice/Assets/css/app.css")
+                .Include("~/Areas/Backoffice/Assets/css/blocks.css")
+                .Include("~/Areas/Backoffice/Assets/css/custom.css")
+                );
 
-            bundles.Add(new ScriptBundle("~/bundles/unify/js")
+            bundles.Add(new ScriptBundle("~/bundles/unify/backoffice/js")
                 .Include("~/Areas/Backoffice/Assets/plugins/bootstrap/js/bootstrap.min.js")
                 .Include("~/Areas/Backoffice/Assets/plugins/back-to-top.js")
                 .Include("~/Areas/Backoffice/Assets/plugins/smoothScroll.js")
+                .Include("~/Areas/Backoffice/Assets/js/app.js")
+                .Include("~/Areas/Backoffice/Assets/js/custom.js")
                 );
         }
     }
---
+++ Src/Web/DotLms.Web/Areas/Backoffice/Assets/js/login-page.js
@@ -0,0 +1,9 @@
+﻿$(document).ready(function () {
+  $.backstretch([
+    "https://elearningindustry.com/wp-content/uploads/2015/11/top-10-cloud-based-learning-management-systems-corporate-training.jpg",
+    "http://www.trainwitheti.com/ETI/wp-content/uploads/2015/03/business-103.jpg",
+  ], {
+    fade: 1000,
+    duration: 7000
+  });
+});
