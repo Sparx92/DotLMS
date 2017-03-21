a17c4a6bcd4a24d5cdf561b56e19f34f0b400e5e
Stefan Koserkov <stefankoserkov@gmail.com>
2017-03-21 15:59:40 +0200

    Adding tests for unit of work.

---
+++ Tests/Data.Tests/DotLms.Data.Tests/DotLmsEfDataUnitTests/ConstructorTests.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DotLms.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace DotLms.Data.Tests.DotLmsEfDataUnitTests
+{
+    
+
+    [TestFixture]
+    [Category(Common.Constants.UnitTestCategory)]
+    public class ConstructorTests
+    {
+        private Mock<IDotLmsEfDbContext> dotLmsEfDbContext;
+
+        [SetUp]
+        public void Init()
+        {
+            this.dotLmsEfDbContext = new Mock<IDotLmsEfDbContext>();
+        }
+
+        [Test]
+        public void Constructor_ShouldThrowArgumentNullException_WhenIDotLmsEfDbContextIsNull()
+        {
+            // Arrange & Act & Assert
+            Assert.Throws<ArgumentNullException>(() => new DotLmsEfData(null));
+        }
+
+        [Test]
+        public void Constructor_ShouldNotThrow_WhenIDotLmsEfDbContextIsNotNull()
+        {
+            // Arrange & Act & Assert
+            Assert.DoesNotThrow(() => new DotLmsEfData(this.dotLmsEfDbContext.Object));
+        }
+    }
+}
---
+++ Tests/Data.Tests/DotLms.Data.Tests/DotLmsEfDataUnitTests/SaveChangesTests.cs
@@ -0,0 +1,46 @@
+﻿using DotLms.Data.Contracts;
+using Moq;
+using NUnit.Framework;
+
+namespace DotLms.Data.Tests.DotLmsEfDataUnitTests
+{
+    [TestFixture]
+    [Category(Common.Constants.UnitTestCategory)]
+    public class SaveChangesTests
+    {
+        private Mock<IDotLmsEfDbContext> dotLmsEfDbContext;
+
+        [SetUp]
+        public void Init()
+        {
+            this.dotLmsEfDbContext = new Mock<IDotLmsEfDbContext>();
+        }
+
+        [Test]
+        public void SaveChanges_ShouldCallDbContextsSaveChanges()
+        {
+            // Arange 
+            DotLmsEfData dotLmsEfData = new DotLmsEfData(dotLmsEfDbContext.Object);
+
+            // Act
+            dotLmsEfData.SaveChanges();
+
+            // Assert
+            this.dotLmsEfDbContext.Verify(x => x.SaveChanges(), Times.Once);
+        }
+
+        [Test]
+        public void SaveChanges_ShouldReturnInt()
+        {
+            // Arange 
+            DotLmsEfData dotLmsEfData = new DotLmsEfData(dotLmsEfDbContext.Object);
+
+            // Act
+            int result = dotLmsEfData.SaveChanges();
+
+            // Assert
+            this.dotLmsEfDbContext.Verify(x => x.SaveChanges(), Times.Once);
+            Assert.AreEqual(typeof(int),result.GetType());
+        }
+    }
+}
