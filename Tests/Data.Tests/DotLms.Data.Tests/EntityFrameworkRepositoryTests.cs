ffc63fc311b8cfb580394ba3b7e3736d2a38bcad
Stefan Koserkov <stefankoserkov@gmail.com>
2017-03-21 15:03:38 +0200

    Adding AAA coments to EntityFramework repository  tests.

--- Tests/Data.Tests/DotLms.Data.Tests/EntityFrameworkRepositoryTests.cs
+++ Tests/Data.Tests/DotLms.Data.Tests/EntityFrameworkRepositoryTests.cs
@@ -31,36 +31,48 @@ namespace DotLms.Data.Tests
         [Test]
         public void Constructor_ShouldThrowArgumentNullException_WhenContextIsNull()
         {
+            // Arrange & Act & Assert
             Assert.Throws<ArgumentNullException>(() => new EntityFrameworkRepository<Course>(null));
         }
 
         [Test]
         public void Constructor_InitializesRepositoryContext()
         {
+            // Arange
             Mock<IDotLmsEfDbContext> mockContext = new Mock<IDotLmsEfDbContext>();
+
+            // Act
             EntityFrameworkRepository<Course> repository = this.GetRepository();
+
+            // Assert  
             Assert.NotNull(repository.Context);
         }
 
         [Test]
         public void Constructor_InitializesRepositoryDbSet()
         {
+            // Arange 
             IDbSet<Course> mockSet = new Mock<IDbSet<Course>>().Object;
             this.context.Setup(x => x.Set<Course>()).Returns(mockSet);
 
+            // Act   
             EntityFrameworkRepository<Course> repository = this.GetRepository();
 
+            // Assert
             Assert.NotNull(repository.DbSet);
         }
 
         [Test]
         public void Constructor_InitializesRepositoryDbSetWithCorrectType()
         {
+            // Arange
             IDbSet<Course> mockSet = new Mock<IDbSet<Course>>().Object;
             this.context.Setup(x => x.Set<Course>()).Returns(mockSet);
 
+            // Act
             EntityFrameworkRepository<Course> repository = this.GetRepository();
 
+            // Assert
             Assert.NotNull(repository.DbSet);
             Assert.IsInstanceOf(typeof(IDbSet<Course>), repository.DbSet);
         }
@@ -68,21 +80,24 @@ namespace DotLms.Data.Tests
         [Test]
         public void All_ReturnsRepositoryDbSet()
         {
+            // Arange
             IDbSet<Course> mockSet = new Mock<IDbSet<Course>>().Object;
 
             this.context.Setup(x => x.Set<Course>()).Returns(mockSet);
 
+            // Act
             EntityFrameworkRepository<Course> repository = this.GetRepository();
 
+            // Assert
             Assert.NotNull(repository.All);
             Assert.IsInstanceOf(typeof(IDbSet<Course>), repository.All);
             Assert.AreSame(repository.All, repository.DbSet);
         }
 
-
         [Test]
         public void Add_ShouldThrowArgumentNullException_WhenNullInputParameterIsPassed()
         {
+            // Arange
             Mock<IDbSet<Course>> mockSet = new Mock<IDbSet<Course>>();
 
             this.context.Setup(x => x.Set<Course>()).Returns(mockSet.Object);
@@ -90,15 +105,18 @@ namespace DotLms.Data.Tests
 
             EntityFrameworkRepository<Course> repository = this.GetRepository();
 
+            // Act & Assert
             Assert.Throws<ArgumentNullException>(() => repository.Add(null));
         }
 
         [Test]
         public void Add_ShouldNotThrow_WhenCorrectInputParameterIsPassed()
         {
+
+            // Arange
             Mock<IDbSet<Course>> mockSet = new Mock<IDbSet<Course>>();
             Mock<Course> mockCourse = new Mock<Course>();
-            DbEntityEntry<Course> fakeEntry = (DbEntityEntry<Course>) FormatterServices
+            DbEntityEntry<Course> fakeEntry = (DbEntityEntry<Course>)FormatterServices
                 .GetSafeUninitializedObject(typeof(DbEntityEntry<Course>));
 
             this.context.Setup(x => x.Set<Course>()).Returns(mockSet.Object);
@@ -107,6 +125,7 @@ namespace DotLms.Data.Tests
 
             EntityFrameworkRepository<Course> repository = this.GetRepository();
 
+            // Act
             try
             {
                 repository.Add(mockCourse.Object);
@@ -118,34 +137,43 @@ namespace DotLms.Data.Tests
                 // DbEntityEntry.State is null
             }
 
+            // Assert
             this.context.Verify(x => x.Entry(mockCourse.Object), Times.AtLeastOnce);
         }
 
         [Test]
         public void DataMaping_Configure()
         {
+            // Arange
             DataMappingsProfile profile = new DataMappingsProfile();
+
+            // Act & Assert
             Assert.DoesNotThrow(() => profile
                 .GetType()
                 .GetMethod("Configure", BindingFlags.NonPublic | BindingFlags.Instance)
-                .Invoke(profile, new object[] {}));
+                .Invoke(profile, new object[] { }));
         }
 
         [Test]
         public void Delete_ShouldThrowArgumentNullException_WhenNullInputParameterIsPassed()
         {
+            // Arange
             Mock<IDbSet<Course>> mockSet = new Mock<IDbSet<Course>>();
 
             this.context.Setup(x => x.Set<Course>()).Returns(mockSet.Object);
             this.context.Setup(x => x.Courses).Returns(mockSet.Object);
 
+            // Act
             EntityFrameworkRepository<Course> repository = this.GetRepository();
+
+            // Assert
             Assert.Throws<ArgumentNullException>(() => repository.Delete(null));
         }
 
         [Test]
         public void Delete_ShouldNotThrow_WhenCorrectInputParameterIsPassed()
         {
+            // Arange
             Mock<IDbSet<Course>> mockSet = new Mock<IDbSet<Course>>();
 
             this.context.Setup(x => x.Set<Course>()).Returns(mockSet.Object);
@@ -154,6 +182,7 @@ namespace DotLms.Data.Tests
             Mock<Course> mockCourse = new Mock<Course>();
             EntityFrameworkRepository<Course> repository = this.GetRepository();
 
+            // Act
             try
             {
                 repository.Delete(mockCourse.Object);
@@ -165,24 +194,29 @@ namespace DotLms.Data.Tests
                 // DbEntityEntry.State is null
             }
 
+            // Assert
             this.context.Verify(x => x.Entry(mockCourse.Object), Times.AtLeastOnce);
         }
 
         [Test]
         public void Update_ShouldThrowArgumentNullException_WhenNullInputParameterIsPassed()
         {
+            // Argange
             Mock<IDbSet<Course>> mockSet = new Mock<IDbSet<Course>>();
 
             this.context.Setup(x => x.Set<Course>()).Returns(mockSet.Object);
             this.context.Setup(x => x.Courses).Returns(mockSet.Object);
-
+            
             EntityFrameworkRepository<Course> repository = this.GetRepository();
+
+            // Act & Assert
             Assert.Throws<ArgumentNullException>(() => repository.Update(null));
         }
 
         [Test]
         public void Update_ShouldNotThrow_WhenCorrectInputParameterIsPassed()
         {
+            // Arange
             Mock<IDbSet<Course>> mockSet = new Mock<IDbSet<Course>>();
 
             this.context.Setup(x => x.Set<Course>()).Returns(mockSet.Object);
@@ -191,6 +225,7 @@ namespace DotLms.Data.Tests
             Mock<Course> mockCourse = new Mock<Course>();
             EntityFrameworkRepository<Course> repository = this.GetRepository();
 
+            // Act
             try
             {
                 repository.Update(mockCourse.Object);
@@ -202,6 +237,7 @@ namespace DotLms.Data.Tests
                 // DbEntityEntry.State is null
             }
 
+            // Assert
             this.context.Verify(x => x.Entry(mockCourse.Object), Times.AtLeastOnce);
         }
 
