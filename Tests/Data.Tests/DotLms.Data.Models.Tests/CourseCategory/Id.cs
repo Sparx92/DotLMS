154084b70c0884f198c85e4b3c03f5c838925707
Stefan Koserkov <stefankoserkov@gmail.com>
2017-03-25 16:20:03 +0200

    Adding some model tests

---
+++ Tests/Data.Tests/DotLms.Data.Models.Tests/CourseCategory/Id.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace DotLms.Data.Models.Tests.CourseCategory
+{
+    [TestFixture]
+    [Category(DotLms.Common.TestConstants.UnitTestCategory)]
+    public class Id
+    {
+        [TestCase(1)]
+        [TestCase(2)]
+        [TestCase(3)]
+        [TestCase(15)]
+        [TestCase(1123)]
+        [TestCase(1123123)]
+        [TestCase(1231)]
+        [TestCase(15)]
+        [TestCase(1312)]
+        public void Id_GetShouldReturnSetValue(int value)
+        {
+            // Arange 
+            Models.CourseCategory category = new Models.CourseCategory();
+
+            // Act
+            category.Id = value;
+
+            // Assert
+            Assert.AreEqual(category.Id,value);
+        }
+    }
+}
